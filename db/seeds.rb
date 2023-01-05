9429c614d4dfe9635a056da4abd69b8e3d950f3c
Boris <95616326+zebre13@users.noreply.github.com>
2023-01-05 14:06:49 +0100

    enriched seed with picture_urls

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@ Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  picture_url: 'https://unsplash.com/fr/photos/178j8tJrNlc',
   price_per_night: 75,
   number_of_guests: 3
 )
@@ -13,6 +14,7 @@ Flat.create!(
   name: 'Stylish House Close to River Thames',
   address: '5 Queensmil Road London SW6 6JP',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  picture_url: 'https://unsplash.com/fr/photos/mR1CIDduGLc',
   price_per_night: 65,
   number_of_guests: 2
 )
@@ -20,6 +22,7 @@ Flat.create!(
   name: 'St Pancras Clock Tower Guest Suite',
   address: 'Euston Rd London N1C 4QP',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  picture_url: 'https://unsplash.com/fr/photos/1ddol8rgUH8',
   price_per_night: 110,
   number_of_guests: 2
 )
@@ -27,6 +30,7 @@ Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  picture_url: 'https://unsplash.com/fr/photos/IYfp2Ixe9nM',
   price_per_night: 75,
   number_of_guests: 3
 )
