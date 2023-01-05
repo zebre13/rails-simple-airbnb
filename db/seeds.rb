192fbbb6a3b53ba0715108c4e6601648b9ee7067
Boris <95616326+zebre13@users.noreply.github.com>
2023-01-05 14:41:24 +0100

    added booking form in show

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  picture_url: 'https://unsplash.com/fr/photos/178j8tJrNlc',
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
   price_per_night: 75,
   number_of_guests: 3
 )
@@ -14,7 +14,7 @@ Flat.create!(
   name: 'Stylish House Close to River Thames',
   address: '5 Queensmil Road London SW6 6JP',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  picture_url: 'https://unsplash.com/fr/photos/mR1CIDduGLc',
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
   price_per_night: 65,
   number_of_guests: 2
 )
@@ -22,7 +22,7 @@ Flat.create!(
   name: 'St Pancras Clock Tower Guest Suite',
   address: 'Euston Rd London N1C 4QP',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  picture_url: 'https://unsplash.com/fr/photos/1ddol8rgUH8',
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
   price_per_night: 110,
   number_of_guests: 2
 )
@@ -30,7 +30,7 @@ Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  picture_url: 'https://unsplash.com/fr/photos/IYfp2Ixe9nM',
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
   price_per_night: 75,
   number_of_guests: 3
 )
