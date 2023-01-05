cd62aa70739961c33214b3e142e107ad1b14e767
Boris <95616326+zebre13@users.noreply.github.com>
2023-01-05 15:08:20 +0100

    small fix

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,7 +3,6 @@ class FlatsController < ApplicationController
 
   def index
     if params[:query].present?
-      @query = params[:query]
       @flats = Flat.where('name LIKE ?', "%#{params[:query]}%")
     else
       @flats = Flat.all
