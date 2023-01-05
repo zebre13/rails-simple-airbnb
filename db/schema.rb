552a7f01e89463d9a851fdaa8fd6232a090a6f58
Boris <95616326+zebre13@users.noreply.github.com>
2023-01-05 13:55:28 +0100

    added picture_url to flats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_081855) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_125321) do
   create_table "flats", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_081855) do
     t.integer "number_of_guests"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_url"
   end
 
 end
