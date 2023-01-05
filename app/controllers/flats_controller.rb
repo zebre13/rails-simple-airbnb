4598f10f43a11b904ef28df3f0276f66bffe2458
Boris <95616326+zebre13@users.noreply.github.com>
2023-01-05 11:44:54 +0100

    update user story with refacto ok

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,10 +1,11 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[show edit update]
+
   def index
     @flats = Flat.all
   end
 
   def show
-    @flat = Flat.find(params[:id])
   end
 
   def new
@@ -20,9 +21,21 @@ class FlatsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
   private
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
